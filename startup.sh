ec3061d405bd2920ca04f375df775f42502a92fc
Sai Gunaranjan <16096792+asgr86@users.noreply.github.com>
2021-09-17 16:40:03 -0700

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+ "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null:q
+
+#install pre requs and Docker
+sudo apt-get update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io
+
+# pull the custom openvpn image
+ sudo docker pull saiguna/openvpn_bionic
+ #run open vpn image
+ sudo docker run --rm -it --cap-add=NET_ADMIN -p 1194:1194/udp -p 943:943/tcp -p 9443:9443/tcp saiguna/openvpn_bionic:latest
