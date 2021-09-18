57399c3cbdf9808b7d050c8968646de28ff80d3b
Sai Gunaranjan <16096792+asgr86@users.noreply.github.com>
2021-09-17 22:44:26 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo su
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
@@ -7,7 +8,7 @@ echo \
 
 #install pre requs and Docker
 sudo apt-get update
-sudo apt-get -y install \
+sudo apt-get -y -q install \
     apt-transport-https \
     ca-certificates \
     curl \
