f037b962b9130c6ecde1062bbb76a10909b8c425
Michael_Jimenez <michaeljimenez@Knights.ucf.edu>
2021-10-03 23:25:01 -0400

    Modified powershell script with DH after meeting

--- deployscript.ps1
+++ deployscript.ps1
@@ -15,26 +15,44 @@ az login --output none
 # Setup RG
 az group create --name openvpn --location westus --output none
 
+# Create secure random password
+$stringAsStream = [System.IO.MemoryStream]::new()
+$writer = [System.IO.StreamWriter]::new($stringAsStream)
+[string]$randValue = Get-Random
+$writer.write("$randValue")
+$writer.Flush()
+$stringAsStream.Position = 0
+[string]$randompass = Get-FileHash -InputStream $stringAsStream | Select-Object Hash -ExpandProperty Hash
+
 # Setup VM with defaults and obtain the newly created machines public IP address
 [string]$data = az vm create --resource-group openvpn --name openvpn --image UbuntuLTS --authentication-type password --admin-username azureuser --admin-password Capstone_8051**
 $json_data = ConvertFrom-JSON -InputObject $data
 $machine_ip = $json_data.publicIpAddress
 
 # Open ports to internet (remove port 22 for final release)
-az vm open-port --port 943,443,22 --resource-group openvpn --name openvpn --output none
+az vm open-port --port 443,22 --resource-group openvpn --name openvpn --output none
+
+# Build up this command syntax
+$command = {\"fileUris\": [\"https://raw.githubusercontent.com/G-PRTCL/startupscripts/main/startup.sh\"],\"commandToExecute\": \"./startup.sh {0} \"} -f $randompass;
+$command = "{"+$command+"}"
+$command = "az vm extension set --resource-group openvpn --vm-name openvpn --name customScript --publisher Microsoft.Azure.Extensions --protected-settings '$command'"
+$command = [scriptblock]::Create("$command")
 
 # Install docker and run openvpn container (Asyncronous version of the command)
-Start-Job -ScriptBlock{az vm extension set --resource-group openvpn --vm-name openvpn --name customScript --publisher Microsoft.Azure.Extensions --protected-settings '{\"fileUris\": [\"https://raw.githubusercontent.com/G-PRTCL/startupscripts/main/startup.sh\"],\"commandToExecute\": \"./startup.sh\"}'}
+Start-Job -ScriptBlock $command
+# Update start up script to move password variable into a file (into the github startup.sh).
+
+# Start-Job -ScriptBlock{ az vm extension set --resource-group openvpn --vm-name openvpn --name customScript --publisher Microsoft.Azure.Extensions --protected-settings $command }
 
 # TODO: We need to check for connectivity, and once connected:
 
 # Add check for this here!
 
 # Pull down the user profile from the openvpn server and launch the client.
-curl.exe -k -u ghost_user:test123 https://${machine_ip}:943/rest/"GetUserlogin" > GPRTCL-profile.ovpn
+curl.exe -k -u ghost_user:"${randompass}" https://${machine_ip}/rest/"GetUserlogin" > GPRTCL-profile.ovpn
 
 # Create a pass.txt file for openvpn login without prompt
-echo "ghost_user`ntest123`n" > pass.txt
+echo "ghost_user`n${randompass}`n" > pass.txt
 
 # login into open vpn using the config file
 openvpn --config ./GPRTCL-profile.ovpn --auth-user-pass pass.txt
