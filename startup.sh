2834c3046f7acd0058844ed49dad0c70037eed30
Mjimenez01 <michaeljimenez@Knights.ucf.edu>
2021-10-05 22:09:03 -0400

    Update startup.sh
    Added additional parameter for self-destruction and also comments.

--- startup.sh
+++ startup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# This script accepts 2 parameters
+# 1) securely generated password
+# 2) self-destruct timer
+
 sudo su
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -19,4 +24,8 @@ sudo apt-get -y -q install \
     containerd.io
  
 # Testing using parameter as environment variable in container.
-sudo docker run --env ghost_pass=$1 --cap-add=NET_ADMIN --device=/dev/net/tun -p 443:443/tcp saiguna/openvpn_focal:latest &
+sudo docker run --env ghost_pass=$1 --env self_destruct_time=$2 --cap-add=NET_ADMIN --device=/dev/net/tun -p 443:443/tcp saiguna/openvpn_focal:latest &
+
+# Shut down after time alloted
+sleep $(($2+60))
+shutdown -h
