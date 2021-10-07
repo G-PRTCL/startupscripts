7a79c7db139b8e166c09c71fac39f915cc82ff3a
Sai Gunaranjan <16096792+asgr86@users.noreply.github.com>
2021-10-07 00:45:01 -0700

    Update deployscript.ps1

--- deployscript.ps1
+++ deployscript.ps1
@@ -1,3 +1,14 @@
+# user prompts
+$rgname = -join ((65..80) + (97..100) | Get-Random -Count 10 | % {[char]$_})
+$vmname = -join ((65..80) + (97..100) | Get-Random -Count 14 | % {[char]$_})
+$randomvmpasswd = -join ((0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random -Count 20  | % {[char]$_}) # The password length must be between 12 and 72. Password must have the 3 of the following: 1 lower case character, 1 upper case character, 1 number and 1 special character.
+#set location based on user input, else defaults to USEast
+$loc = Read-Host -Prompt "Provide the location to host your VPN service [defaults to useast]"
+if ([string]::IsNullOrWhiteSpace($loc))
+{
+$loc = "useast2"
+}
+
 # Install chocolately
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
@@ -10,10 +21,11 @@ choco install azure-cli -y # TODO: Make sure to either refresh the shell to get 
 # 3) az login ...
 
 # Azure login
-az login --output none
+#az login --output none
+
 
 # Setup RG
-az group create --name openvpn --location westus --output none
+az group create --name $rgname --location $loc --output none
 
 # Create secure random password
 $stringAsStream = [System.IO.MemoryStream]::new()
@@ -22,10 +34,10 @@ $writer = [System.IO.StreamWriter]::new($stringAsStream)
 $writer.write("$randValue")
 $writer.Flush()
 $stringAsStream.Position = 0
-[string]$randompass = Get-FileHash -InputStream $stringAsStream | Select-Object Hash -ExpandProperty Hash
+#[string]$randomvmpasswd = Get-FileHash -InputStream $stringAsStream | Select-Object Hash -ExpandProperty Hash
 
 # Setup VM with defaults and obtain the newly created machines public IP address
-[string]$data = az vm create --resource-group openvpn --name openvpn --image UbuntuLTS --authentication-type password --admin-username azureuser --admin-password Capstone_8051**
+[string]$data = az vm create --resource-group $rgname --name $vmname --image UbuntuLTS --size Standard_DS1_v2 --authentication-type password --admin-username $vmname.ToLower() --admin-password $randomvmpasswd # not working on free student account --priority spot --eviction-policy Delete 
 $json_data = ConvertFrom-JSON -InputObject $data
 $machine_ip = $json_data.publicIpAddress
 
@@ -60,11 +72,12 @@ Start-Job -ScriptBlock $command
 While (!(Test-Path .\GPRTCL-profile.ovpn -ErrorAction SilentlyContinue)){
     # Pull down the user profile from the openvpn server and launch the client.
     curl.exe -k -u ghost_user:"${randompass}" https://${machine_ip}/rest/"GetUserlogin" -o GPRTCL-profile.ovpn
+    System.Io.File]::ReadAllText(.\GPRTCL-profile.ovpn) | Out-File -FilePath ./GPRTCL-profile.ovpn -Encoding Ascii
     sleep 1
 }
 
 # Create a pass.txt file for openvpn login without prompt
-echo "ghost_user`n${randompass}`n" > pass.txt
+#echo "ghost_user`n${randompass}`n" > pass.txt
 
 # login into open vpn using the config file
 openvpn --config ./GPRTCL-profile.ovpn --auth-user-pass pass.txt
