a644ef7e8bf862b99aec86b5801efface472f4f4
Sai Gunaranjan <16096792+asgr86@users.noreply.github.com>
2021-10-13 00:01:34 -0700

    Update deployscript.ps1
    fix VDI

--- deployscript.ps1
+++ deployscript.ps1
@@ -130,7 +130,7 @@ $command = [scriptblock]::Create("$command")
 Start-Job -ScriptBlock $command
 # deploys windows machine into existing network, keeping the VM in the same RG to enable self destruct 
 # TODO: make this a optional based on user input
-[string]$winvm = az vm create --resource-group $rgname --name $winvmname --vnet-name $json_network.name --subnet $json_network.subnets.name --image Win2019Datacenter --authentication-type password --admin-username $vmname.ToLower() --admin-password $randomvmpasswd --accelerated-networking true # not supported for student accounts --priority spot --eviction-policy Delete --encryption-at-host true
+[string]$winvm = az vm create --resource-group $rgname --name $winvmname --vnet-name $json_network.name --subnet $json_network.subnets.name --image Win2019Datacenter --admin-username $vmname.ToLower() --admin-password $randomvmpasswd # not supported for student accounts --priority spot --eviction-policy Delete --encryption-at-host true
 $json_winvm = ConvertFrom-JSON -InputObject $winvm
 $win_machine_ip = $json_winvm.publicIpAddress
 
